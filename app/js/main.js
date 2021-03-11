fd1fbc71bcc79ab416f80b3ac4c17d8f4f9ea7db
Pjery3 <mixxer1265755@gmail.com>
2021-03-11 21:48:55 +0300

    added about

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,16 @@
-$(function(){
+$(function () {
     $('.header-item__wrapper').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: false,
         arrows: true,
-      });
+    });
+    $('.about-item').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    });
 });
