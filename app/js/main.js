79daa256117cdadd4c530f7bb8c53a65a54afd1b
Pjery3 <mixxer1265755@gmail.com>
2021-03-11 15:43:04 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.header-item__wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+      });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ function images() {
 
 function scripts() {
     return src([
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js',
     ])
         .pipe(concat('main.min.js'))
@@ -46,7 +47,10 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src([
+        'node_modules/slick-carousel/slick/slick.css',
+        'app/scss/style.scss',
+    ])
         .pipe(scss({ outputStyle: 'compressed' }))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
