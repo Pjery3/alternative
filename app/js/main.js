67990d771198f86fa4f0889a5ffe22665039f928
Pjery3 <mixxer1265755@gmail.com>
2021-03-14 19:56:09 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -24,12 +24,79 @@ $(function () {
         slidesToScroll: 4,
         dots: false,
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 930,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
     $('.footer-photos').slick({
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 830,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+
+                }
+                
+            },
+            {
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+        ]
     });
     $('.btn-groups .btn').on('click', function (e) {
         $('.btn-groups .btn').removeClass('active');
@@ -41,5 +108,11 @@ $(function () {
         $('.footer-photos').slick('slickUnfilter');
         $('.footer-photos').slick('slickFilter', $(`.footer-photo${filterClass}`).closest('.slick-slide'));
     });
+    $('').on('breakpoint', function () {
+        var filterClass = $(this).data('filter');
+        $('.footer-photos').slick('slickFilter', $(`.footer-photo${filterClass}`).closest('.slick-slide'));
+        $('.footer-photos').slick('slickUnfilter');
+    });
+    
 });
 
